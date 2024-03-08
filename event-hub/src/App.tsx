cdfdc90bf1892145144186ef3440280fc593656b
Zoltan Miklos <zoltan.miklos@nokia.com>
2024-03-08 15:24:50 +0100

    React App: Building a responsive layout

--- event-hub/src/App.tsx
+++ event-hub/src/App.tsx
@@ -1,8 +1,28 @@
-import './App.css'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import "./App.css";
+import { Button, ButtonGroup, Show } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme='blue'>Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`, // mobile view
+        lg: `"nav nav" "aside main"`, // 1024px or larger devices
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        {" "}
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
-
-export default App
+export default App;
