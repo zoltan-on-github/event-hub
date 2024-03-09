8bb2b9d2803de54245882e0812c5c681ff7c5d28
Zoltan Miklos <zoltan.miklos@nokia.com>
2024-03-09 13:49:21 +0100

    React App: Build the colormode switch

--- event-hub/src/components/NavBar.tsx
+++ event-hub/src/components/NavBar.tsx
@@ -1,14 +1,15 @@
 import { Box, HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import logo from "../assets/noun-dj-3181685.svg";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Box bg="blue.200 ">
         <Image src={logo} boxSize="60px" />
       </Box>
-      <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
