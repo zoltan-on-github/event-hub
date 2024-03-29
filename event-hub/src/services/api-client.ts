ced609c272945bc28f616c4ad5c459e068208ef0
zoltan-on-github <zoltanegymasikcime@gmail.com>
2024-03-29 12:26:17 +0100

    React App: Update production API Server settings

--- event-hub/src/services/api-client.ts
+++ event-hub/src/services/api-client.ts
@@ -4,7 +4,11 @@ const production = import.meta.env.PROD
 
 var API_ENDPOINT = 'http://localhost:3000'
 if (production) {
-    API_ENDPOINT = 'http://localhost:8081'
+    // custom JSON Server container
+    //API_ENDPOINT = 'http://localhost:8081'
+
+    // JSON Server hosted on typicode.com
+    API_ENDPOINT = 'https://my-json-server.typicode.com/zoltan-on-github/event-hub-data'
 }
 
 export default axios.create({baseURL: API_ENDPOINT})
